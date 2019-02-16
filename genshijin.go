e7dd665fd6cd19941d9c84798f3dd34f8be206ae
kitagawa <kitadrum50@gmail.com>
2019-02-16 14:24:56 +0900

    スピードアップ シ タ

--- genshijin.go
+++ genshijin.go
@@ -6,9 +6,12 @@ import (
 	"github.com/ikawaha/kagome/tokenizer"
 )
 
+var (
+	t tokenizer.Tokenizer = tokenizer.New()
+)
+
 // ToGenshijin s ニューリョク ゲンシ ジンゴ カエス
 func ToGenshijin(s string) (res string) {
-	t := tokenizer.New()
 	tokens := t.Tokenize(s)
 
 	for _, token := range tokens {
